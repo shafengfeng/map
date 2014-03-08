a0c09b3a4e1719cfac7ef360af4a51fd7879bfb4
shafengfeng <shafengfeng2008@126.com>
2014-03-08 22:47:48 +0800

    Initial commit

---
+++ src/paixu.java
@@ -0,0 +1,25 @@
+/**
+ * Created by shafengfeng on 14-3-6.
+ */
+
+import com.sun.org.apache.xpath.internal.operations.*;
+
+import java.lang.String;
+import java.util.*;
+
+public class paixu {
+
+    public String getmap(Map<String,String> ma)
+    {
+        return null;
+    }
+
+    public  static  void main(String[] args)
+    {
+        System.out.println("你妈逼！");
+    }
+
+}
+
+
+
